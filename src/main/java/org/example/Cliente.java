f1645cff565cffc28e9a100ce65744bec6b75c75
thiagovirmes <138067813+thiagovirmes@users.noreply.github.com>
2025-11-04 20:33:07 -0300

    Teste do Hibernate
    Terminei a configuração do hibernate e fiz o teste com a classe cliente

--- src/main/java/org/example/Cliente.java
+++ src/main/java/org/example/Cliente.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false, unique = true)
     private String cpf;
 
     public Long getId() {
